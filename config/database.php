d008c390e2b8bef5830a3b5876f3d6f0af63d14f
Tatianna Torres <tat8@njit.edu>
2019-04-29 20:18:02 -0400

    3.  Create a database seed that inserts 50 users

--- config/database.php
+++ config/database.php
@@ -17,21 +17,22 @@ return [
 
     'default' => env('DB_CONNECTION', 'mysql'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
-    */
+
+/*
+|--------------------------------------------------------------------------
+| Database Connections
+|--------------------------------------------------------------------------
+|
+| Here are each of the database connections setup for your application.
+| Of course, examples of configuring each database platform that is
+| supported by Laravel is shown below to make development simple.
+|
+|
+| All database work in Laravel is done through the PHP PDO facilities
+| so make sure you have the driver for your particular database of
+| choice installed on your machine before you begin development.
+|
+*/
 
     'connections' => [
 
@@ -85,16 +86,6 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-        ],
-        'pg-heroku' => [
-            'driver' => 'pgsql',
-            'host' => $heroku_db_url['host'],
-            'database' => substr($heroku_db_url['path'], 1),
-            'username' => $heroku_db_url['user'],
-            'password' => $heroku_db_url['pass'],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
         ],
 
     ],
