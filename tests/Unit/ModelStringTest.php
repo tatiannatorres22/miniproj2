e4918aae1eb8b73fa7c67c8ba3af3516b5bfb581
Tatianna Torres <tat8@njit.edu>
2019-05-10 12:50:10 -0400

    15. Create a unit tests to check that the model is a string

---
+++ tests/Unit/ModelStringTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+
+class ModelStringTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        #factory(Car::class)->create();
+        $car =  Car::first();
+        $this->assertInternalType("string", $car->model);
+    }
+}
