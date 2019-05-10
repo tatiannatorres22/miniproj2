e12bb4e64e1245f3b189602e06793f062c1988d3
Tatianna Torres <tat8@njit.edu>
2019-05-10 11:56:00 -0400

    10. Create a unit test to test updating a car year to 2000

---
+++ tests/Unit/UpdatingCarYearTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+
+class UpdatingCarYearTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        #$car = factory(Car::class)->create();
+        $car =  Car::first();
+        #dd($car);
+        $this->assertTrue(is_string($car->year));
+    }
+}
